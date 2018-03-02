1977dd6f0b25d5bde161e7ef83d0eaffdcad68ef
tangj1206 <1798516284@qq.com>
2018-03-02 11:17:01 +0800

    定义表结构

--- Blog/app.js
+++ Blog/app.js
@@ -11,7 +11,7 @@ var app = express();
 
 //设置静态文件托管
 // 当用户访问的url以/public开始,那么直接返回对应__dirname + '/public'下的文件
-app.use('/public',express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 //配置模板
 //定义当前应用使用的模板引擎
@@ -29,10 +29,16 @@ swig.setDefaults({
 });
 
 //根据不同的功能划分模块
-app.use('/admin',require('./routers/admin'));
-app.use('/api',require('./routers/api'));
-// app.use('/',require('./routers/main'));
+app.use('/admin', require('./routers/admin'));
+app.use('/api', require('./routers/api'));
+app.use('/', require('./routers/main'));
 
-mongoose.connect();
-//监听app请求
-app.listen(8081);
+mongoose.connect('mongodb://localhost:27018/blog', function (err) {
+    if (err) {
+        console.log('数据库链接失败');
+    } else {
+        console.log('数据库链接成功');
+        //监听app请求
+        app.listen(8081);
+    }
+});
