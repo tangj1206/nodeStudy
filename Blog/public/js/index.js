5719f6c6dc8f33a1ba8e9f8e4a32cb7b24528a9d
tangj1206 <1798516284@qq.com>
2018-03-05 17:36:50 +0800

    基于数据库的验证和用户信息验证

---
+++ Blog/public/js/index.js
@@ -0,0 +1,43 @@
+$(function () {
+    
+    var $loginBox = $('#loginBox');
+    var $registerBox = $('#registerBox');
+
+    //切换到注册面板
+    $loginBox.find('a.colMint').on('click',function(){
+        $registerBox.show();
+        $loginBox.hide();
+    });
+    
+    //切换到登录面板
+    $registerBox.find('a.colMint').on('click',function(){
+        $loginBox.show();
+        $registerBox.hide();
+    });
+
+    //注册
+    $registerBox.find('button').on('click',function () {
+        //通过ajax提交请求
+        $.ajax({
+            type: 'post',
+            url:'/api/user/register',
+            data:{
+                username:$registerBox.find('[name="username"]').val(),
+                password:$registerBox.find('[name="password"]').val(),
+                repassword:$registerBox.find('[name="repassword"]').val()
+            },
+            dataType:'json',
+            success:function (result) {
+                $registerBox.find('.colWarning').html(result.message);
+
+                if (!result.code) {
+                    //注册成功
+                    setTimeout(function () {
+                        $registerBox.hide();
+                        $loginBox.show();
+                    },1000)
+                }
+            }
+        })
+    });
+})
--- Blog/routers/api.js
+++ Blog/routers/api.js
@@ -1,5 +1,71 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
+//统一返回格式
+var responseData;
+router.use(function (req, res, next) {
+    responseData = {
+        code: 0,
+        message: ''
+    };
+    next();
+});
 
+//用户注册
+router.post('/user/register', function (req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var repassword = req.body.repassword;
+
+    //用户是否为空
+    if (username == '') {
+        responseData.code = 1;
+        responseData.message = '用户名不能为空';
+        res.json(responseData);
+    }
+
+    //密码是否为空
+    if (password == '') {
+        responseData.code = 1;
+        responseData.message = '密码不能为空';
+        res.json(responseData);
+    }
+
+    //重复密码是否为空
+    if (repassword == '') {
+        responseData.code = 1;
+        responseData.message = '重复密码不能为空';
+        res.json(responseData);
+    }
+
+    //两次输入的密码是否一致
+    if (repassword != password) {
+        responseData.code = 1;
+        responseData.message = '两次输入密码不一致'
+        res.json(responseData);
+    }
+
+    //用户名是否已经被注册了，如果数据库中已经存在和我们要注册的用户名同名的数据，则表示该用户名被注册了
+    User.findOne({
+        username: username
+    }).then(function (userInfo) {
+        if (userInfo) {
+            //表示数据库有该记录
+            responseData.code = 4;
+            responseData.message = '用户名已经被注册';
+            res.json(responseData);
+            return ;
+        }
+        //保存用户数据信息
+        var user = new User({
+            username: username,
+            password: password
+        });
+        return user.save();
+    }).then(function (newUserInfo) {
+        responseData.message = '用户注册成功';
+        res.json(responseData);
+    });
+});
 module.exports = router;
