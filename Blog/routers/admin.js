75d7867c9d941b6a521a033c772e2e1da3692f3d
tangj1206 <1798516284@qq.com>
2018-03-06 17:38:15 +0800

    后台管理-注册用户数据分页原理和实现

--- Blog/routers/admin.js
+++ Blog/routers/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var User = require('../models/User');
 
-router.use(function (req, res ,next) {
+router.use(function (req, res, next) {
     if (!req.userInfo.isAdmin) {
         //如果当前用户是非管理员
         res.send('对不起，只有管理员才可以进入后台管理');
@@ -12,20 +12,40 @@ router.use(function (req, res ,next) {
     next();
 })
 
-router.get('/',function (req, res, next) {
-    res.render('admin/index',{
-        userInfo:req.userInfo
+router.get('/', function (req, res, next) {
+    res.render('admin/index', {
+        userInfo: req.userInfo
     });
 });
 
 //用户管理
-router.get('/user',function(req, res, next){
+router.get('/user', function (req, res, next) {
 
     //从数据库读取所有用户数据
-    User.find().then(function (users) {
-        res.render('admin/user_index',{
-            userInfo:req.userInfo,
-            users:users
+    // limit(number):限制获取的数据条数
+    //skip(number):忽略数据的条数
+
+    var page = Number(req.query.page) || 1;
+    var limit = 2;
+    var pages = 0;
+
+    User.count().then(function (count) {
+
+        //计算总页数
+        pages = Math.ceil(count / limit);
+        //取值不能超过pages
+        page = Math.min(page, pages);
+        //取值不能小于1
+        page = Math.max(page, 1);
+
+        var skip = (page - 1) * limit;
+        
+        User.find().limit(limit).skip(skip).then(function (users) {
+            res.render('admin/user_index', {
+                userInfo: req.userInfo,
+                users: users,
+                page: page
+            });
         });
     });
 });
