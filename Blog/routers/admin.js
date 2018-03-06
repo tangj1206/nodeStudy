6ce1d4ae8bcabd3c3b4b50aca7e18fa142570933
tangj1206 <1798516284@qq.com>
2018-03-06 16:10:05 +0800

    后台管理功能及界面的搭建

--- Blog/routers/admin.js
+++ Blog/routers/admin.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function (req, res ,next) {
+    if (!req.userInfo.isAdmin) {
+        //如果当前用户是非管理员
+        res.send('对不起，只有管理员才可以进入后台管理');
+        return;
+    }
+    next();
+})
 
+router.get('/',function (req, res, next) {
+    res.render('admin/index',{
+        userInfo:req.userInfo
+    });
+});
 module.exports = router;
