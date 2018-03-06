69e3043f418f7be5fe4e577b41f518d1f7ba7be2
tangj1206 <1798516284@qq.com>
2018-03-06 11:07:54 +0800

    用户登录

--- Blog/public/js/index.js
+++ Blog/public/js/index.js
@@ -1,33 +1,34 @@
 $(function () {
-    
+
     var $loginBox = $('#loginBox');
     var $registerBox = $('#registerBox');
+    var $userInfo = $('#userInfo');
 
     //切换到注册面板
-    $loginBox.find('a.colMint').on('click',function(){
+    $loginBox.find('a.colMint').on('click', function () {
         $registerBox.show();
         $loginBox.hide();
     });
-    
+
     //切换到登录面板
-    $registerBox.find('a.colMint').on('click',function(){
+    $registerBox.find('a.colMint').on('click', function () {
         $loginBox.show();
         $registerBox.hide();
     });
 
     //注册
-    $registerBox.find('button').on('click',function () {
+    $registerBox.find('button').on('click', function () {
         //通过ajax提交请求
         $.ajax({
             type: 'post',
-            url:'/api/user/register',
-            data:{
-                username:$registerBox.find('[name="username"]').val(),
-                password:$registerBox.find('[name="password"]').val(),
-                repassword:$registerBox.find('[name="repassword"]').val()
+            url: '/api/user/register',
+            data: {
+                username: $registerBox.find('[name="username"]').val(),
+                password: $registerBox.find('[name="password"]').val(),
+                repassword: $registerBox.find('[name="repassword"]').val()
             },
-            dataType:'json',
-            success:function (result) {
+            dataType: 'json',
+            success: function (result) {
                 $registerBox.find('.colWarning').html(result.message);
 
                 if (!result.code) {
@@ -35,7 +36,35 @@ $(function () {
                     setTimeout(function () {
                         $registerBox.hide();
                         $loginBox.show();
-                    },1000)
+                    }, 1000)
+                }
+            }
+        })
+    });
+    //登录
+    $loginBox.find('button').on('click', function () {
+        //通过ajax提交请求
+        $.ajax({
+            type: 'post',
+            url: '/api/user/login',
+            data: {
+                username: $loginBox.find('[name="username"]').val(),
+                password: $loginBox.find('[name="password"]').val()
+            },
+            dataType: 'json',
+            success: function (result) {
+                $loginBox.find('.colWarning').html(result.message);
+
+                if (!result.code) {
+                    //登录成功
+                    setTimeout(function () {
+                        $loginBox.hide();
+                        $userInfo.show();
+
+                        //显示登录用户的信息
+                        $userInfo.find('.username').html(result.userInfo.username);
+                        $userInfo.find('.info').html('您好，欢迎光临我的博客');
+                    }, 1000)
                 }
             }
         })
