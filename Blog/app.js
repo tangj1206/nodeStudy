adad87c345382f5344bbb0f384abd8569530eb38
tangj1206 <1798516284@qq.com>
2018-03-06 15:21:03 +0800

    普通用户和管理员功能的实现

--- Blog/app.js
+++ Blog/app.js
@@ -13,6 +13,9 @@ var Cookies = require('cookies');
 //创建app应用
 var app = express();
 
+var User = require('./models/User');
+
+
 //设置静态文件托管
 // 当用户访问的url以/public开始,那么直接返回对应__dirname + '/public'下的文件
 app.use(express.static('public'));
@@ -32,21 +35,30 @@ swig.setDefaults({
     cache: false
 });
 //body-parser设置
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 //设置cookie
 app.use(function (req, res, next) {
     req.cookies = new Cookies(req, res);
 
+    //解析登录用户的cookie信息
     req.userInfo = {};
-
     if (req.cookies.get('userInfo')) {
         try {
             req.userInfo = JSON.parse(req.cookies.get('userInfo'));
-        } catch (error) {
-            console.log(error);
+
+            //获取当前登录用户的类型，是否是管理员
+            User.findById(req.userInfo._id).then(function (userInfo) {
+                req.userInfo.isAdmin = Boolean(userInfo.isAdmin);
+                next();
+            })
+        } catch (e) {
+            next();
         }
+    }else{
+        next();
     }
-    return next();
 });
 
 //根据不同的功能划分模块
