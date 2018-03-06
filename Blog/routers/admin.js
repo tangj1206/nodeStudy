25c03138c46197935e96eaec1f970543f24a80e7
tangj1206 <1798516284@qq.com>
2018-03-06 16:35:37 +0800

    后台管理-注册用户数据展示

--- Blog/routers/admin.js
+++ Blog/routers/admin.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/User');
+
 router.use(function (req, res ,next) {
     if (!req.userInfo.isAdmin) {
         //如果当前用户是非管理员
@@ -13,6 +15,18 @@ router.use(function (req, res ,next) {
 router.get('/',function (req, res, next) {
     res.render('admin/index',{
         userInfo:req.userInfo
+    });
+});
+
+//用户管理
+router.get('/user',function(req, res, next){
+
+    //从数据库读取所有用户数据
+    User.find().then(function (users) {
+        res.render('admin/user_index',{
+            userInfo:req.userInfo,
+            users:users
+        });
     });
 });
 module.exports = router;
