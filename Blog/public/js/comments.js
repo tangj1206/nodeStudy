128ecc375405048e10eb8c73dbb1879a1e73f29f
tangj1206 <1798516284@qq.com>
2018-03-15 16:03:16 +0800

    评论分页

--- Blog/public/js/comments.js
+++ Blog/public/js/comments.js
@@ -11,6 +11,11 @@ $(function () {
         }
     })
 
+
+    var perpage = 2;
+    var page = 1;
+    var pages = 0;
+
     //提交评论
     $('#messageBtn').on('click', function () {
         $.ajax({
@@ -29,16 +34,32 @@ $(function () {
 
 
     function renderComment(comments) {
+
+        $('#messageCount').html(comments.length);
+
+        var pages = Math.ceil(comments.length / perpage);
+        var start = (page-1) * perpage;
+        var end = start + perpage;
+
+        var $li = $('.pager li');
+        $li.eq(1).html(page + '/' + pages);
+
         var html = '';
-        for (let i = 0; i < comments.length; i++) {
+        for (let i = start; i < end; i++) {
             html += `<div class="messageBox">
                         <p class="name clear">
                             <span class="fl">`+comments[i].username+`</span>
-                            <span class="fr">`+comments[i].postTime+`</span>
+                            <span class="fr">`+formatDate(comments[i].postTime)+`</span>
                         </p>
                         <p>`+comments[i].content+`</p>
                     </div>`;
         }
-        $('#messageList').append(html);
+        $('#messageList').html(html);
+    }
+
+    function formatDate(d) {
+        var date1 = new Date(d);
+        return date1.getFullYear() + '年' + (date1.getMonth()+1) + '月' + date1.getDate() + '日' + 
+        date1.getHours() + ':' + date1.getMinutes() + ':' + date1.getSeconds();
     }
 })
